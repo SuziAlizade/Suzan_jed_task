29bbddaaa03c18234e4dd93c609742dc67649bfd
Suzi Alizade <elizade.suzan@gmail.com>
2022-10-28 22:37:10 +0400

    Added my project

---
+++ js_tasks/jS.Task1/task1.js
@@ -0,0 +1,19 @@
+// 1. area_of_rectangle
+let width = Number(prompt("Please enter width of rectangle")) ; 
+let length = Number(prompt("Please enter length of rectangle")); 
+ 
+console.log(`area_of_rectangle: ${width*length}`);
+
+
+// 2. age_calculator
+
+let birthDate = Number(prompt("Please enter your birth date")) 
+let year = Number(prompt("Please enter year you are in now")) 
+console.log(`age_calculator: ${year - birthDate}`)
+
+// 3. km to mil
+
+let km = Number(prompt("Enter the number with km")) 
+let mil = '0.6214' 
+alert(`${km} km equal to ${km * mil} mil`)
+
