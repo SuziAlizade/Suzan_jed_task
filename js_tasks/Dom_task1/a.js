d4cf96181c2eaa125127eba8513773c2c65b4ae6
Suzi Alizade <110241376+SuziAlizade@users.noreply.github.com>
2022-11-24 15:04:16 +0400

    Add files via upload

---
+++ js_tasks/Dom_task1/a.js
@@ -0,0 +1,5 @@
+
+document.getElementById("redButton").onclick = () => document.getElementById("container").style.backgroundColor='red';
+document.getElementById("blueButton").onclick = () => document.getElementById("container").style.backgroundColor='blue';
+document.getElementById("yellowButton").onclick = () => document.getElementById("container").style.backgroundColor='yellow';
+document.getElementById("grayButton").onclick = () => document.getElementById("container").style.backgroundColor='gray';
